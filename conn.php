88680f6724e789d35169a9008caaf26af7efe806
christian mpimbaza <xtianm4@gmail.com>
2018-08-20 20:14:43 +0300

    connection

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("localhost","root","","sample");
+$conn = mysqli_connect("localhost","root","root#123","sample");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
